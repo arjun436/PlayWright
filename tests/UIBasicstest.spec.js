7b4e6e4704151b794d6efbad6ceb0d6a902a5147
arjun436 <arjun.miryala@hotmail.com>
2025-12-28 21:14:52 +0530

    network ideal start

--- tests/UIBasicstest.spec.js
+++ tests/UIBasicstest.spec.js
@@ -1,7 +1,7 @@
 const {test, expect} = require('@playwright/test');
 
 // browser is globally available object it is a fixture
-test.only('Page Playwright test', async ({page}) => {
+test('Page Playwright test', async ({page}) => {
     const userName = page.locator('input#username');
     const password = page.locator('input#password');
     const signInBtn = page.locator('#signInBtn');
@@ -41,4 +41,28 @@ test('Browser context', async ({browser}) => {
     // get the title - assertion
     console.log( await page.title());
     await expect(page).toHaveTitle("Google");
+});
+
+test.only('Assignment', async ({page}) => {
+    const userName = page.locator('input#userEmail');
+    const password = page.locator('input#userPassword');
+    const signInBtn = page.locator('input#login');
+    const cardTitles = page.locator(".card-body b");
+
+    await page.goto("https://rahulshettyacademy.com/client");
+    await userName.fill("arjun.miryala36@gmail.com");
+    await password.fill("Test1234");
+    await signInBtn.click();
+
+    await expect(page).toHaveTitle("Let's Shop");
+
+    // console.log( await cardTitles.first().textContent());
+    // console.log(await cardTitles.last().textContent());
+
+    //wait for network idle state so we can see all the cards are loaded. no need to wait for first element to be loaded
+    await page.waitForLoadState('networkidle');
+ 
+    let allTitles = await cardTitles.allTextContents();
+    console.log(allTitles);
+
 });
